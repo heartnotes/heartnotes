ecc984769fe749ce8a6e6e50f48ab95a3a5516e0
Ramesh Nair <ram@hiddentao.com>
2015-04-17 19:33:48 +0800

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,23 @@
+"use strict";
+
+$(function() {
+
+  // ensure map fills window
+  var mapElem = $('#map'),
+    $window = $(window);
+
+  var _resizeMap = function() {
+    mapElem.height($window.height() - $('nav.topbar').height());    
+  };
+  _resizeMap();
+
+  // if window size changes resize the map
+  $window.resize(_resizeMap);
+
+  var mapOptions = {
+    center: { lat: 39.5, lng: -108.35 },
+    zoom: 4
+  };
+
+  var map = new google.maps.Map($('#map').get(0), mapOptions);
+});
