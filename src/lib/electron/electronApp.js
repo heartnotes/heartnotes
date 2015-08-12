24e98812e815f8208d2e08cbaab46097b0573453
Ramesh Nair <ram@hiddentao.com>
2015-08-12 21:34:29 +0800

    dont open devtools automatically

--- src/lib/electron/electronApp.js
+++ src/lib/electron/electronApp.js
@@ -34,7 +34,7 @@ app.on('ready', function() {
   mainWindow.loadUrl(url);
 
   // Open the devtools.
-  mainWindow.openDevTools();
+  // mainWindow.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
