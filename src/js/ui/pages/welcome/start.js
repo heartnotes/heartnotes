0cc086b2f82fb4915afe3e7c1dcc54a576589370
Ramesh Nair <ram@hiddentao.com>
2015-12-19 09:58:57 +0000

    work towards new start screen

--- src/js/ui/pages/welcome/start.js
+++ src/js/ui/pages/welcome/start.js
@@ -1,12 +1,8 @@
 var _ = require('lodash'),
   React = require('react');
 
-import SelectBox from 'react-select-box';
 
-
-var Detect = require('../../../utils/detect'),
-  StringUtils = require('../../../utils/string'),
-  Button = require('../../components/button'),
+var Button = require('../../components/button'),
   IconButton = require("../../components/iconButton"),
   Popup = require("../../components/popup"),
   PasswordInput = require('../../components/passwordInput'),
@@ -25,40 +21,41 @@ var Component = React.createClass({
 
   getInitialState: function() {
     return {
+      username: null,
       password: null,
     };
   },
 
   render: function() { 
-    let { opening, lastOpenedDiary, availableDiaries } = this.props.data.diary;
+    let { opening } = this.props.data.diary;
 
-    var content = null;
+    let username = this._getUsername();
 
-    if (_.values(availableDiaries).length) {
-      var lastDiaryName = !lastOpenedDiary ? null : StringUtils.formatDiaryName(
-        lastOpenedDiary.name
-      );
+    let buttonAttrs = {
+      onClick: this._openDiary,
+      animActive: !!opening.inProgress,
+      disabled: !_.get(this.state.password, 'length') || !_.get(this.state.username, 'length'),
+    };
 
-      var buttonAttrs = {
-        onClick: this._openDiary,
-        animActive: !!opening.inProgress,
-      };
-
-      if (!this.state.password || !this.state.password.length) {
-        buttonAttrs.disabled = true;
-      }
-
-      let passwordInputClassName = lastDiaryName ? 'open' : 'close';
-
-      content = (
+    return (
+      <div className="start step">
         <div className="open-existing">
-          <div>
-            <label>Diary:</label>
-            {this._buildChooseAnotherDiaryButton()}
-          </div>
+          <div><label>Open diary</label></div>
           <form onSubmit={this._openDiary}>
-            <div className={`field row ${passwordInputClassName}`}>
-              <PasswordInput password={this.state.password} onChange={this._setPassword} />
+            <div className="field row">
+              <input type="text"
+                ref="username"
+                onInput={this._setUsername} 
+                value={username} 
+                placeholder="Email address"
+                tabIndex={1} />
+            </div>
+            <div className="field row">
+              <PasswordInput 
+                placeholder="Password"
+                password={this.state.password} 
+                onChange={this._setPassword} 
+                tabIndex={2} />
             </div>
             <div className="action row">
               <OpenDiaryProgressPopup {...this.props}>
@@ -67,45 +64,18 @@ var Component = React.createClass({
             </div>
           </form>
         </div>
-      );
-    }
-
-    content = (
-      <div>
-        {content}
         <div className="create-new">
           <Button onClick={this._createNew}>Create new diary</Button>
         </div>
       </div>
     );
-
-    return (
-      <div className="start step">
-        {content}
-      </div>
-    );
   },
 
 
+  _getUsername: function() {
+    let { lastOpenedDiary } = this.props.data.diary;
 
-  _buildChooseAnotherDiaryButton: function() {
-    let { lastOpenedDiary, availableDiaries } = this.props.data.diary;
-
-    let options = _.map(availableDiaries, (name, id) => {
-      return (
-        <option key={id} value={id}>{name}</option>
-      );
-    });
-
-    return (
-      <SelectBox 
-        label="Choose diary"
-        className="choose-diary-selector"
-        onChange={this._chooseDiary}
-        value={_.get(lastOpenedDiary, 'id', null)}>
-          {options}
-      </SelectBox>
-    );
+    return this.state.username || _.get(lastOpenedDiary, 'username', '');
   },
 
 
@@ -113,13 +83,19 @@ var Component = React.createClass({
     e.preventDefault();
 
     this.props.actions.openDiary(
-      this.props.data.diary.lastOpenedDiary.id,
+      this._getUsername(),
       this.state.password
     )
       .then(() => {
         this.props.showStep('loadDiary');
       });
   },
+
+
+  _createNew: function() {
+    this.props.showStep('newDiary');
+  },
+
 
 
   _setPassword: function(p) {
@@ -129,13 +105,16 @@ var Component = React.createClass({
   },
 
 
-  _createNew: function() {
-    this.props.showStep('newDiary');
+  _setUsername: function(e) {
+    let name = $(e.currentTarget).val();
+
+    this.setState({
+      username: name,
+    });
   },
 
-  _chooseDiary: function(id) {    
-    this.props.actions.chooseDiary(id);
-  },
+
+
 
 });
 
